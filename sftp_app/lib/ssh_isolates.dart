357a8d5a8aaa59c79ec787bb3f58b9ce0bb6ee65
SpindlySpider <kaihrmt@gmail.com>
2023-11-26 22:47:06 +0000

    initial write up of isolates threads

--- sftp_app/lib/ssh_isolates.dart
+++ sftp_app/lib/ssh_isolates.dart
@@ -8,63 +8,68 @@ import "my_bindings.dart";
 import "ssh.dart";
 
 void isolate_ssh_initilize(SendPort sendPort){
+  //communication through lists, [message, ?object]
   Pointer ssh = "".toNativeUtf8();
   ReceivePort receivePort = ReceivePort();
   Pointer ssh_sesh =init_ssh();
   Pointer<Utf8> error_message = calloc.allocate<Utf8>(250) ;
+  int host; 
+  receivePort.listen((message) {
+    if(message[0] == "set_connection_info"){
+      //message lists are used to send data of host name and port
+      set_connection_info(message[1], message[2], ssh_sesh, error_message);
+      sendPort.send(["error",error_message.toDartString()]);
+      error_message = "".toNativeUtf8();
+    }
+    else if(message[0] == "verify_host"){
+      host = verify_host(ssh_sesh,error_message);
 
-  set_connection_info(hostname, port, ssh_sesh, error_message);
-            int host; 
-            status_message = error_message.toDartString();
-            print(error_message.toDartString());
-            error_message = "".toNativeUtf8();
-            host = verify_host(ssh_sesh,error_message);
+      sendPort.send(["error",error_message.toDartString()]);
+      print(error_message.toDartString());
 
-            status_message = error_message.toDartString();
-            print(error_message.toDartString());
+      host = verify_host(ssh_sesh,error_message);
 
-            print(host);
-            if(host<0){
-              if (host == -2){
-                //run pop up code here
-                // this is yes to the unknown hosts need to add y/n funcitonality
-                sSH_KNOWN_HOSTS_UNKOWN_handle(ssh_sesh, error_message);
-                print(error_message.toDartString());
-                status_message = error_message.toDartString();
-                //if user wants to accept use host = 0
-                host = 0;
-                }
+      sendPort.send(["error",error_message.toDartString()]);
+      print(error_message.toDartString());
 
-            else{
-              print(error_message.toDartString());
-              setState(() {
-                status_message = error_message.toDartString() + ",ending session";
-                
-              });
-            //give popup to quit 
-                // my_ssh_disconnect(ssh_sesh);
-                // my_ssh_free(ssh_sesh);
-                // ssh_sesh = nullptr;
-                // calloc.free(error_message);
+      print(host);
+      if(host<0){
+        if (host == -2){
+          //run pop up code here
+          // this is yes to the unknown hosts need to add y/n funcitonality
+          sSH_KNOWN_HOSTS_UNKOWN_handle(ssh_sesh, error_message);
+          print(error_message.toDartString());
+          sendPort.send(["error",error_message.toDartString()]);;
+          //if user wants to accept use host = 0
+          host = 0;
+          }
 
-              }
-            }
-            if (host != 0){
-                // my_ssh_disconnect(ssh_sesh);
-                // my_ssh_free(ssh_sesh);
-                // ssh_sesh = nullptr;
-                // calloc.free(error_message);
-              //exit
-            }
-            else{
+      else{
+        print(error_message.toDartString());
+        sendPort.send(["error",error_message.toDartString()+ ",ending session"]);
+      //give popup to quit 
+          // my_ssh_disconnect(ssh_sesh);
+          // my_ssh_free(ssh_sesh);
+          // ssh_sesh = nullptr;
+          // calloc.free(error_message);
 
+        }
+      }
+      if (host != 0){
+          // my_ssh_disconnect(ssh_sesh);
+          // my_ssh_free(ssh_sesh);
+          // ssh_sesh = nullptr;
+          // calloc.free(error_message);
+        //exit
+      }
+    }
+    else if(message[0] == "try_password_authentication"){
+      //[password_auth, password]
           try_password_authentication(ssh_sesh, "RjHRL4v8",error_message);
-          status_message = error_message.toDartString();
-          status_message = "SUCCESS";              
-          calloc.free(error_message);
-            }
-
-
-
-
+          // status_message = error_message.toDartString();
+          // status_message = "SUCCESS";
+          sendPort.send(["error",error_message.toDartString()]);
+    }
+}
+);
 }
