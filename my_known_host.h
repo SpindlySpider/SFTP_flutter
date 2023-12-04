e8741cf40a19a6dd2ddb22d44cb399860b564022
SpindlySpider <kaihrmt@gmail.com>
2023-12-04 14:11:28 +0000

    removed c backend

--- my_known_host.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_KNOWN_HOST_H
-#define MY_KNOWN_HOST_H
-#include <libssh/libssh.h>
-
-int verify_knownhost(ssh_session session,char* error_message);
-int SSH_KNOWN_HOSTS_UNKOWN_handle(ssh_session ssh_sesh,char* error_message);
-#endif
